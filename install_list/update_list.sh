be5fc833a86f254a39aad5a22e604f22450a478e
Masaya Kamei <km9smc6@gmail.com>
2021-08-15 16:39:55 +0900

    install_listを更新

--- install_list/update_list.sh
+++ install_list/update_list.sh
@@ -2,3 +2,5 @@
 
 # コマンドはパスを指定する
 /usr/local/bin/code --list-extensions > ~/dotfiles/install_list/extensions_list
+# launchcd で自動実行すると、mas が記録されない
+/usr/local/bin/brew bundle dump --force --file ~/dotfiles/install_list/Brewfile
