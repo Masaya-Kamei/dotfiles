f93cf801d9988b17663c8ed89b539be470f6a59f
mkamei <mkamei@student.42tokyo.jp>
2022-02-03 16:48:51 +0900

    Edit link.sh

--- link.sh
+++ link.sh
@@ -13,7 +13,7 @@ for v in "${vscode_settings[@]}"; do
 done
 ln -svf ~/dotfiles/vscode/style.less ~/.mume/
 
-karabiner_settings=(ctrl_enter.json)
+karabiner_settings=(ctrl_enter.json chrome_new.json chrome_search.json stickies_new.json stickies_append.json)
 for k in "${karabiner_settings[@]}"; do
 	ln -svf ~/dotfiles/karabiner/$k ~/.config/karabiner/assets/complex_modifications/
 done
